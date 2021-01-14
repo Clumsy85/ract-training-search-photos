9c2b8de3e5706c2c689269503c072347df9a91de
Clumsy85 <37352599+Clumsy85@users.noreply.github.com>
2021-01-14 12:42:04 +0200

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID OQ3uskA5SZasnF9cry5RHiwwajEBEO9dmSeyd5CfWPk'
+        Authorization: 'Client-ID Your_Token_Here'
     }
 });
